f70b3dddf9b314f6fe01473121b01d8dbb996814
dtalleycgr <157645869+dtalleycgr@users.noreply.github.com>
2024-12-03 13:26:22 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,16 +37,16 @@ public class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 1;
-    public static final int kRearLeftDrivingCanId = 3;
-    public static final int kFrontRightDrivingCanId = 5;
-    public static final int kRearRightDrivingCanId = 7;
+    public static final int kFrontLeftDrivingCanId = 10;
+    public static final int kRearLeftDrivingCanId = 12;
+    public static final int kFrontRightDrivingCanId = 14;
+    public static final int kRearRightDrivingCanId = 16;
 
 
-    public static final int kFrontLeftTurningCanId = 2;
-    public static final int kRearLeftTurningCanId = 4;
-    public static final int kFrontRightTurningCanId = 6;
-    public static final int kRearRightTurningCanId = 8;
+    public static final int kFrontLeftTurningCanId = 11;
+    public static final int kRearLeftTurningCanId = 13;
+    public static final int kFrontRightTurningCanId = 15;
+    public static final int kRearRightTurningCanId = 17;
 
     public static final boolean kGyroReversed = false;
   }
